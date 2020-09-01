ccfd96249107035c32d3b7f3194eab0901355ecf
Navya Garg <34911308+navgarg@users.noreply.github.com>
2020-09-01 12:23:13 +0530

    Update GetRides.py

--- Lambda Functions/GetRides.py
+++ Lambda Functions/GetRides.py
@@ -5,12 +5,6 @@ from boto3.dynamodb.conditions import Key
 def lambda_handler(event, context):
 	client = boto3.resource('dynamodb')
 	table = client.Table('Rides')
-	response = table.get_item(
-		Key={
-			'user_id': 'E0VJnE0YitPFQhy25F3efPD1PXt2',
-			'datetime': '2020-07-18'
-		}
-	)
 	userid = event['user_id']
 	date_time = event['datetime']
 	response = table.query(
